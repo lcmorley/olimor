29c6b840576dcaeb047589eb5ff67ae141c2130f
Leon Morley <Leon Morley>
2017-01-02 09:19:43 +0000

    Amend poms

--- spring-boot-example/src/main/java/uk/co/olimor/service/RestService.java
+++ spring-boot-example/src/main/java/uk/co/olimor/service/RestService.java
@@ -1,14 +1,16 @@
 package uk.co.olimor.service;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value="OlimorController")
+@RestController
 public class RestService {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Blah Boot!";
+	@RequestMapping(method = RequestMethod.GET, 
+			value = "/test")
+	public String getAccountSummary() {
+		return "Account summary.";
 	}
 
 }
