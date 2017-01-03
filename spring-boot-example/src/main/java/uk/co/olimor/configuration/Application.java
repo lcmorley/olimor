7118c99273f2e907bdf3117bac8c040161ae40d3
Leon Morley <Leon Morley>
2017-01-03 08:36:58 +0000

    make the war deployable

--- spring-boot-example/src/main/java/uk/co/olimor/configuration/Application.java
+++ spring-boot-example/src/main/java/uk/co/olimor/configuration/Application.java
@@ -2,11 +2,18 @@ package uk.co.olimor.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"uk.co.olimor.service"})
-public class Application {
+@ComponentScan({ "uk.co.olimor.service" })
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
